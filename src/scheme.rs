5d6b493ff62966fbd79f9bcaf5b9ca5d0aeb59d2
MarcoSigno03 <87857643+MarcoSigno03@users.noreply.github.com>
2025-01-26 18:08:31 +0100

    Update scheme.rs

--- src/scheme.rs
+++ src/scheme.rs
@@ -1,6 +1,7 @@
+use dronegowski_utils::network::SimulationControllerNodeType;
 use eframe::egui;
 use eframe::egui::{Color32, Pos2, Stroke};
-use crate::{NodeType, DronegowskiSimulationController};
+use crate::{DronegowskiSimulationController};
 
 impl DronegowskiSimulationController {
     pub fn central_panel(&mut self, ui: &mut egui::Ui) {
@@ -9,16 +10,6 @@ impl DronegowskiSimulationController {
         painter.rect_filled(response.rect, 0.0, background_color);
 
         let panel_offset = response.rect.min;
-
-        // Disegna un rettangolo cliccabile in alto a sinistra
-        let rect1 = egui::Rect::from_center_size(
-            Pos2::new(10.0 + panel_offset.x, 10.0 + panel_offset.y),
-            egui::vec2(30.0, 30.0),
-        );
-        painter.rect_filled(rect1, 0.0, Color32::from_black_alpha(50));
-        if ui.allocate_rect(rect1, egui::Sense::click()).clicked() {
-            self.left_panel = !self.left_panel;
-        }
 
         // Disegna connessioni e nodi
         for elem in &self.nodi {
@@ -37,9 +28,9 @@ impl DronegowskiSimulationController {
 
         for elem in &mut self.nodi {
             let fill_color = match elem.node_type {
-                NodeType::SERVER => Color32::LIGHT_RED,
-                NodeType::CLIENT => Color32::LIGHT_GREEN,
-                NodeType::DRONE => Color32::LIGHT_BLUE,
+                SimulationControllerNodeType::SERVER => Color32::LIGHT_RED,
+                SimulationControllerNodeType::CLIENT => Color32::LIGHT_GREEN,
+                SimulationControllerNodeType::DRONE => Color32::LIGHT_BLUE,
             };
 
             let rect = egui::Rect::from_center_size(
@@ -57,9 +48,9 @@ impl DronegowskiSimulationController {
             );
 
             let letter = match elem.node_type {
-                NodeType::SERVER => "S",
-                NodeType::CLIENT => "C",
-                NodeType::DRONE => "D",
+                SimulationControllerNodeType::SERVER => "S",
+                SimulationControllerNodeType::CLIENT => "C",
+                SimulationControllerNodeType::DRONE => "D",
             };
 
             painter.text(
