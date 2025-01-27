d51f9fbdf775537cadef5096d62d18db039012ab
MarcoSigno03 <87857643+MarcoSigno03@users.noreply.github.com>
2025-01-27 18:31:13 +0100

    Update scheme.rs

--- src/scheme.rs
+++ src/scheme.rs
@@ -6,6 +6,11 @@ use crate::{DronegowskiSimulationController};
 
 impl DronegowskiSimulationController {
     pub fn central_panel(&mut self, ui: &mut egui::Ui) {
+        // Variabile statica per memorizzare l'ultimo nodo cliccato
+        thread_local! {
+            static LAST_CLICKED_NODE: std::cell::RefCell<Option<NodeId>> = std::cell::RefCell::new(None);
+        }
+
         let (response, painter) = ui.allocate_painter(ui.available_size(), egui::Sense::click_and_drag());
         let background_color = Color32::GRAY;
         painter.rect_filled(response.rect, 0.0, background_color);
@@ -13,21 +18,27 @@ impl DronegowskiSimulationController {
         let panel_offset = response.rect.min;
         let pointer_position = ui.input(|i| i.pointer.interact_pos());
 
-        // Disegna connessioni e nodi
+        // Determina il nodo cliccato
+        let mut clicked_node_id: Option<NodeId> = None;
+
         for elem in &self.nodi {
-            for &neighbour in &elem.neighbours {
-                if let Some(neighbour_node) = self.nodi.iter().find(|node| node.node_id == neighbour) {
-                    painter.line_segment(
-                        [
-                            Pos2::new(elem.xy.0 + panel_offset.x, elem.xy.1 + panel_offset.y),
-                            Pos2::new(neighbour_node.xy.0 + panel_offset.x, neighbour_node.xy.1 + panel_offset.y),
-                        ],
-                        Stroke::new(2.0, Color32::BLACK),
-                    );
+            let position = Pos2::new(elem.xy.0 + panel_offset.x, elem.xy.1 + panel_offset.y);
+            if let Some(pointer) = pointer_position {
+                let distance = position.distance(pointer);
+                if distance <= 30.0 && ui.input(|i| i.pointer.any_click()) {
+                    clicked_node_id = Some(elem.node_id);
                 }
             }
         }
 
+        // Aggiorna il nodo cliccato
+        if let Some(node_id) = clicked_node_id {
+            LAST_CLICKED_NODE.with(|last_clicked| {
+                *last_clicked.borrow_mut() = Some(node_id);
+            });
+        }
+
+        // Disegna i nodi
         for elem in &mut self.nodi {
             let rect = egui::Rect::from_center_size(
                 Pos2::new(elem.xy.0 + panel_offset.x, elem.xy.1 + panel_offset.y),
@@ -37,24 +48,18 @@ impl DronegowskiSimulationController {
 
             let position = Pos2::new(elem.xy.0 + panel_offset.x, elem.xy.1 + panel_offset.y);
 
-            // Determina se questo nodo è cliccato in base alla posizione del puntatore e al clic
-            let is_node_clicked = if let Some(pointer) = pointer_position {
-                let distance = position.distance(pointer);
-                distance <= 30.0 && ui.input(|i| i.pointer.any_click())
-            } else {
-                false
-            };
-
-            // Imposta il colore del nodo
-            let fill_color = if is_node_clicked {
-                Color32::YELLOW // Nodo cliccato evidenziato
-            } else {
-                match elem.node_type {
-                    SimulationControllerNodeType::SERVER { .. } => Color32::LIGHT_RED,
-                    SimulationControllerNodeType::CLIENT { .. } => Color32::LIGHT_GREEN,
-                    SimulationControllerNodeType::DRONE { .. } => Color32::LIGHT_BLUE,
+            // Determina il colore del nodo
+            let fill_color = LAST_CLICKED_NODE.with(|last_clicked| {
+                if *last_clicked.borrow() == Some(elem.node_id) {
+                    Color32::YELLOW // Nodo cliccato evidenziato
+                } else {
+                    match elem.node_type {
+                        SimulationControllerNodeType::SERVER { .. } => Color32::LIGHT_RED,
+                        SimulationControllerNodeType::CLIENT { .. } => Color32::LIGHT_GREEN,
+                        SimulationControllerNodeType::DRONE { .. } => Color32::LIGHT_BLUE,
+                    }
                 }
-            };
+            });
 
             painter.circle(
                 position,
