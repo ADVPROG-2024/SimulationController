84558f1a42eaf9b16a53d8f5cd3ef68d769123d3
MarcoSigno03 <87857643+MarcoSigno03@users.noreply.github.com>
2025-02-08 17:57:21 +0100

    Update bottom_left_panel.rs

--- src/bottom_left_panel.rs
+++ src/bottom_left_panel.rs
@@ -1,21 +1,47 @@
 use std::fmt::Debug;
+use dronegowski_utils::hosts::ClientEvent;
 use eframe::egui;
+use wg_2024::controller::DroneEvent;
 use crate::DronegowskiSimulationController;
 
 impl DronegowskiSimulationController<'_> {
     pub fn bottom_left_panel(&mut self, ui: &mut egui::Ui){
         for elem in &self.panel.bottom_left_panel.drone_event{
-            ui.label(format!("{:?}", elem));
+            match elem {
+                DroneEvent::PacketSent(..) =>{
+                    ui.label("Drone received packet sent");
+                }
+                DroneEvent::PacketDropped(_) => {
+                    ui.label("Drone received packet dropped");
+
+                }
+                DroneEvent::ControllerShortcut(_) => {
+                    ui.label("Drone received controller shortcut");
+
+                }
+            }
         }
 
         for elem in &self.panel.bottom_left_panel.client_event{
-            ui.label(format!("{:?}", elem));
+            match elem {
+                ClientEvent::ServerTypeReceived(server_id, server_type) => {
+                    ui.label(format!("Client {} received 'Im {:?}' from Server {:?}", self.panel.central_panel.selected_node.clone().unwrap().node_id, server_type, server_id));
+                }
+                ClientEvent::ClientListReceived(server_id, clients) => {}
+                ClientEvent::FilesListReceived(server_id, files) => {}
+                ClientEvent::FileReceived(server_id, file_data) => {}
+                ClientEvent::MediaReceived(server_id, media_data) => {}
+                ClientEvent::MessageFromReceived(server_id, from_id, message) => {}
+                ClientEvent::RegistrationOk(server_id) => {}
+                ClientEvent::RegistrationError(server_id) => {}
 
+                ClientEvent::PacketSent(_) => {}
+                ClientEvent::MessageReceived(_) => {}
+            }
         }
 
         for elem in &self.panel.bottom_left_panel.server_event{
             //ui.label(format!("{:?}", elem));
-
         }
     }
 }
