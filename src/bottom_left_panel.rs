a2dfdc2c4c4eb71ca6ae23c7163e269f866bb034
Sam Nejati <sam4nzd@gmail.com>
2025-06-19 23:06:38 +0200

    UI vertical

--- src/bottom_left_panel.rs
+++ src/bottom_left_panel.rs
@@ -212,7 +212,7 @@ impl DronegowskiSimulationController<'_> {
 
     /// Helper function to display a drone-related notification in the UI.
     pub fn print_drone_notify(&self, text: String, ui: &mut egui::Ui) {
-        ui.horizontal(|ui| {
+        ui.vertical(|ui| {
             ui.add_space(8.);
             ui.label(RichText::new(text).size(15.0).color(Color32::LIGHT_BLUE));
         });
@@ -221,7 +221,7 @@ impl DronegowskiSimulationController<'_> {
 
     /// Helper function to display a client-related notification in the UI.
     pub fn print_client_notify(&self, text: String, ui: &mut egui::Ui) {
-        ui.horizontal(|ui| {
+        ui.vertical(|ui| {
             ui.add_space(8.);
             ui.label(RichText::new(text).size(15.0).color(Color32::LIGHT_GREEN));
         });
@@ -230,7 +230,7 @@ impl DronegowskiSimulationController<'_> {
 
     /// Helper function to display a server-related notification in the UI.
     pub fn print_server_notify(&self, text: String, ui: &mut egui::Ui) {
-        ui.horizontal(|ui| {
+        ui.vertical(|ui| {
             ui.add_space(8.);
             ui.label(RichText::new(text).size(15.0).color(Color32::LIGHT_RED));
         });
