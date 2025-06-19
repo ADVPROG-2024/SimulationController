f1e9f0591aab188282ccca66e4c82639e6322640
MarcoSigno03 <87857643+MarcoSigno03@users.noreply.github.com>
2025-06-19 23:55:22 +0200

    Update bottom_left_panel.rs

--- src/bottom_left_panel.rs
+++ src/bottom_left_panel.rs
@@ -212,28 +212,46 @@ impl DronegowskiSimulationController<'_> {
 
     /// Helper function to display a drone-related notification in the UI.
     pub fn print_drone_notify(&self, text: String, ui: &mut egui::Ui) {
+<<<<<<< Updated upstream
         ui.vertical(|ui| {
             ui.add_space(8.);
+=======
+        ui.add_space(4.0);
+        ui.vertical(|ui| {
+            ui.add_space(8.0);
+>>>>>>> Stashed changes
             ui.label(RichText::new(text).size(15.0).color(Color32::LIGHT_BLUE));
         });
-        ui.add_space(8.);
+        ui.add_space(4.0);
     }
 
     /// Helper function to display a client-related notification in the UI.
     pub fn print_client_notify(&self, text: String, ui: &mut egui::Ui) {
+<<<<<<< Updated upstream
         ui.vertical(|ui| {
             ui.add_space(8.);
+=======
+        ui.add_space(4.0);
+        ui.vertical(|ui| {
+            ui.add_space(8.0);
+>>>>>>> Stashed changes
             ui.label(RichText::new(text).size(15.0).color(Color32::LIGHT_GREEN));
         });
-        ui.add_space(8.);
+        ui.add_space(4.0);
     }
 
     /// Helper function to display a server-related notification in the UI.
     pub fn print_server_notify(&self, text: String, ui: &mut egui::Ui) {
+<<<<<<< Updated upstream
         ui.vertical(|ui| {
             ui.add_space(8.);
+=======
+        ui.add_space(4.0);
+        ui.vertical(|ui| {
+            ui.add_space(8.0);
+>>>>>>> Stashed changes
             ui.label(RichText::new(text).size(15.0).color(Color32::LIGHT_RED));
         });
-        ui.add_space(8.);
+        ui.add_space(4.0);
     }
 }
